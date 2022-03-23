7d4e75966023af1ad212de21e03d4c514471f519
Yun <bangga0403@gmail.com>
2022-03-23 15:00:17 +0900

    Add Game Model

---
+++ src/model/Game.java
@@ -0,0 +1,12 @@
+package Tetris.src.model;
+
+import javax.swing.Timer;
+
+public class Game {
+    float currentScore;
+    //Block nextBlock;
+    int[][] currentBlock;
+    int[][] currentboard;
+    //private int initInterval = 1000;
+    Timer frame;
+}
