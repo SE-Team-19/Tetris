b2a6a21532d607d0fe16a3d099515ba035b13a90
Yun <bangga0403@gmail.com>
2022-04-02 01:11:30 +0900

    Fix Setting to be Serializable

--- app/src/main/java/tetris/model/Setting.java
+++ app/src/main/java/tetris/model/Setting.java
@@ -1,16 +1,69 @@
 package tetris.model;
 
-public class Setting {
+import java.io.Serializable;
+
+public class Setting implements Serializable {
+
+  static final long serialVersionUID = 3518731767529258119L;
   private int displayMode;
   private boolean colorBlindMode;
-  private String moveLeftKey;
-  private String moveRightKey;
-  private String rotateKey;
-  private String stackKey;
+  private int moveLeftKey;
+  private int moveRightKey;
+  private int rotateKey;
+  private int stackKey;
 
-  public Setting(int dispalyMode, boolean colorBlindMode, String moveLeftKey, String moveRightKey,
-      String rotateKey, String stackKey) {
+  public int getDisplayMode() {
+    return this.displayMode;
+  }
+
+  public void setDisplayMode(int displayMode) {
+    this.displayMode = displayMode;
+  }
+
+  public boolean isColorBlindMode() {
+    return this.colorBlindMode;
+  }
+
+  public void setColorBlindMode(boolean colorBlindMode) {
+    this.colorBlindMode = colorBlindMode;
+  }
+
+  public int getMoveLeftKey() {
+    return this.moveLeftKey;
+  }
+
+  public void setMoveLeftKey(int moveLeftKey) {
+    this.moveLeftKey = moveLeftKey;
+  }
+
+  public int getMoveRightKey() {
+    return this.moveRightKey;
+  }
+
+  public void setMoveRightKey(int moveRightKey) {
+    this.moveRightKey = moveRightKey;
+  }
+
+  public int getRotateKey() {
+    return this.rotateKey;
+  }
+
+  public void setRotateKey(int rotateKey) {
+    this.rotateKey = rotateKey;
+  }
+
+  public int getStackKey() {
+    return this.stackKey;
+  }
+
+  public void setStackKey(int stackKey) {
+    this.stackKey = stackKey;
+  }
+
+  public Setting(int dispalyMode, boolean colorBlindMode, int moveLeftKey, int moveRightKey,
+      int rotateKey, int stackKey) {
     this.displayMode = dispalyMode;
+    this.colorBlindMode = colorBlindMode;
     this.moveLeftKey = moveLeftKey;
     this.moveRightKey = moveRightKey;
     this.rotateKey = rotateKey;
