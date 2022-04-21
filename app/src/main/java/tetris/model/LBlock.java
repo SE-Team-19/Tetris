b07e3ee44eec48a79635451a36c314adc3fada01
YoungJun-L <71017759+YoungJun-L@users.noreply.github.com>
2022-04-22 08:48:57 +0900

    Feat/game controller (#28)
    * FEAT: Create ViewController
    
    * Add MainKeyEvent to ViewController for receving keyborad inputs
    
    * [WIP]Implement GameView,MainView,ScoreView and SettingView
    
    * Fix package name for SonarLint
    
    * Fix Syntax by GoogleStyleSheet
    
    * Fix feat/view permissions for gradlew
    
    * Fix GameView to follow Singleton pattern
    
    * Fix MainView to follow Singleton pattern
    
    * Fix SettingView to follow Singleton pattern
    
    * Fix ScoreView to follow Singleton pattern
    
    * Fix ViewController for view's Singleton pattern
    
    * Fix Setting to be Serializable
    
    * Add save and load Method to SettingController
    
    * Add '.gitignore' file
    
    * Revise Block.java contents
    
    * Add function and revise comment
    
    * FEAT: Create GameBoard (not completed) #1
    
    * FEAT: Add GameController(not completed) #1
    
    * Move Block classes to GameController
    
    * Remove 'GameBoard' part
    
    * Feat: Delete Board(not used)
    
    * Backup before starting work; 4/15
    
    * Implement Layout of SettingView
    
    * Add KeyListner for SettingView changes
    
    * Error: Add GameView and Block, but not executed
    
    * Add initAndSetName method for test
    
    * Fix getter name in addSettingViewEvent Method
    
    * Add SettingView test code
    
    * FEAT: Modification of the overall structure of the game part
    
    * Revise typo
    
    * Block modification
    
    * FEAT: Add functions
    
    * Add drawBlockColor, drawNextBlockColor to revise game-border
    
    * Add controller to save json
    
    * Refactor gameView
    
    * Fix the game screen to run
    
    * Add gitignore
    
    * Add function to remove overlapping display in next block
    
    * Move setStyle function to GameController
    
    * add Styles and accumulated function(not finished)
    
    * Remove 'final' keyword
    
    * Add linecheck methods and other methods
    
    * Add views to feat/game
    
    * Add StackMethod at GameController
    
    * Add copyline method at GameController
    
    * Fix Overlap nextBlock
    
    * Fix color change if block is accumlated
    
    * Add GameOverText at GameController
    
    * Add GameControllerTest
    
    * Add randomBlockGenerator to gameController
    
    * Fix Collision bug
    
    * Merge feat/game into feat/gameController
    
    * Add dialog for to receive name
    
    * Add game speed logic for mode
    
    * Add TrainsitView method at GameController
    
    * Add transitView to GameController
    
    * load ScoreBoard
    
    Co-authored-by: Moojun <80478750+Moojun@users.noreply.github.com>
    Co-authored-by: Yun <bangga0403@gmail.com>
    Co-authored-by: moojun <mgim32137@gmail.com>

---
+++ app/src/main/java/tetris/model/LBlock.java
@@ -0,0 +1,13 @@
+package tetris.model;
+
+import java.awt.Color;
+
+public class LBlock extends Block {
+
+    public LBlock() {
+        shape = new int[][] { { 1, 0 }, { 1, 0 }, { 1, 1 } };
+        color = Color.BLUE;
+        blindColor = Color.BLUE;
+        indentifynumber = 4;
+    }
+}
